42a8a7fac74627e65f38ba305f246179ffeeb37b
TiagoDiass <tiago.costadiasss@gmail.com>
2021-10-25 20:54:11 -0300

    feat: add bootstrap and refactor initial structure

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+
+    <button type="button" class="btn btn-primary">MY BUTTON</button>
+
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Vue from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default Vue.extend({
-  name: "Home",
+  name: 'Home',
   components: {
     HelloWorld,
   },
