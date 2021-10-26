75e46e31f40da8d5fbacdb8d834df04a3f43f118
TiagoDiass <tiago.costadiasss@gmail.com>
2021-10-25 21:44:47 -0300

    feat: add Sidebar component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
 <template>
   <div id="app">
-    <router-view />
+    <Sidebar />
+
+    <div id="content-container">
+      <router-view />
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Sidebar } from '@/components';
+
+export default Vue.extend({
+  components: {
+    Sidebar,
+  },
+});
+</script>
---
+++ src/components/Sidebar/Sidebar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="sidebar">
+    <h2 class="logo"><span class="light">Front</span>TCC</h2>
+
+    <div id="nav">
+      <a @click="navigateTo('Home')"><i class="fas fa-home" /> Home</a>
+      <a @click="navigateTo('Pacientes')">
+        <i class="fas fa-users" /> Pacientes
+      </a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'sidebar',
+
+  methods: {
+    navigateTo(name: string): void {
+      this.$router.push({ name });
+    },
+  },
+};
+</script>
+
+<style lang="scss" src="./Sidebar.scss" />
