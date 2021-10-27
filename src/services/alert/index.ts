8526ebbc8a3ac437f9e2c3c7b779cb398c8c5ff9
TiagoDiass <tiago.costadiasss@gmail.com>
2021-10-26 22:24:43 -0300

    feat: add sweet alert and alert abstraction

---
+++ src/services/alert/index.ts
@@ -0,0 +1,70 @@
+import sweetAlert, { SweetAlertResult } from 'sweetalert2';
+
+const success = (text: string): Promise<SweetAlertResult<unknown>> =>
+  sweetAlert.fire({
+    icon: 'success',
+    title: 'Sucesso',
+    text,
+  });
+
+const error = (text: string): Promise<SweetAlertResult<unknown>> =>
+  sweetAlert.fire({
+    icon: 'error',
+    title: 'Ops...',
+    text,
+  });
+
+type AlertInfoParams = {
+  title: string;
+  text: string;
+};
+
+const info = ({
+  text,
+  title,
+}: AlertInfoParams): Promise<SweetAlertResult<unknown>> =>
+  sweetAlert.fire({
+    icon: 'info',
+    title,
+    text,
+  });
+
+type AlertQuestionParams = {
+  text: string;
+  title?: string;
+  focusCancelButton?: boolean;
+  cancelButtonText?: string;
+  confirmButtonText?: string;
+};
+
+const question = async ({
+  text,
+  title = 'Tem certeza?',
+  cancelButtonText = 'Cancelar',
+  focusCancelButton = true,
+  confirmButtonText = 'OK',
+}: AlertQuestionParams): Promise<{ answer: 'yes' | 'no' }> => {
+  const result = await sweetAlert.fire({
+    icon: 'warning',
+    title,
+    text,
+    showCancelButton: true,
+    cancelButtonText,
+    cancelButtonColor: 'var(--danger)',
+    focusCancel: focusCancelButton,
+    confirmButtonText,
+  });
+
+  return {
+    answer: result.value ? 'yes' : 'no',
+  };
+};
+
+const alert = {
+  success,
+  error,
+  info,
+  question,
+};
+
+export default alert;
---
+++ src/views/Animals/Animals.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="animals view">
+    <div class="view-header">
+      <h1>Animais</h1>
+
+      <p>
+        Este é o módulo de
+        <span class="text-primary text-bold">Pacientes</span>, onde você poderá
+        fazer a gestão de todos os pacientes
+      </p>
+    </div>
+
+    <div class="view-content d-flex flex-column" style="row-gap: 12px">
+      <h1>shit shit</h1>
+      <button class="btn btn-info" @click="launchAlert1">
+        lançar alerta questão
+      </button>
+      <button class="btn btn-info" @click="launchAlert2">
+        lançar alerta sucesso
+      </button>
+      <button class="btn btn-info" @click="launchAlert3">
+        lançar alerta erro
+      </button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+// import swal from 'sweetalert2';
+import alert from '@/services/alert';
+
+export default Vue.extend({
+  methods: {
+    async launchAlert1() {
+      const { answer } = await alert.question({
+        text: 'Após deletar um paciente, os dados dele não poderão ser recuperados',
+        focusCancelButton: false,
+      });
+
+      if (answer === 'yes') {
+        alert.success('Deletado');
+      } else {
+        alert.info({ title: 'Tudo bem', text: 'Não foi deletado' });
+      }
+    },
+
+    launchAlert2() {
+      alert.success('SUCESSO MEU QUERIDOOOO');
+    },
+
+    launchAlert3() {
+      alert.error('Deu ruim meu compatriota');
+    },
+  },
+});
+</script>
+
+<style></style>
