26cd71e2c9c0a8171dde04f454d714719d6f5657
TiagoDiass <tiago.costadiasss@gmail.com>
2021-10-27 09:59:37 -0300

    test: ensure Sidebar navigates to correct route if user clicks on a link, add coverage to .gitignore

--- src/components/Sidebar/Sidebar.test.ts
+++ src/components/Sidebar/Sidebar.test.ts
@@ -1,12 +1,20 @@
 import Sidebar from './Sidebar.vue';
 import sidebarOptions from './sidebarOptions';
-import { render, screen } from '@testing-library/vue';
+import { render, screen, waitFor } from '@testing-library/vue';
 import userEvent from '@testing-library/user-event';
 
 const renderComponent = async () => {
-  render(Sidebar);
+  const $routerMock = {
+    push: jest.fn(),
+  };
+
+  render(Sidebar, { mocks: { $router: $routerMock } });
 
   await screen.findByLabelText('Barra de navegação lateral');
+
+  return {
+    $routerMock,
+  };
 };
 
 describe('Component: Sidebar', () => {
@@ -19,8 +27,16 @@ describe('Component: Sidebar', () => {
   });
 
   it('should navigate to the correct route when user clicks on a link', async () => {
-    await renderComponent();
+    const { $routerMock } = await renderComponent();
+    const linkOption = sidebarOptions[3];
 
-    userEvent.click(screen.getByText('Animais'));
+    userEvent.click(screen.getByText(linkOption.label));
+
+    await waitFor(() => {
+      expect($routerMock.push).toHaveBeenCalledTimes(1);
+      expect($routerMock.push).toHaveBeenCalledWith({
+        name: linkOption.routeName,
+      });
+    });
   });
 });
