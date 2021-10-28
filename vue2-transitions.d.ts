f4a5252b533ea9bb1ee8c62d26af2fbdaeb8d7e3
TiagoDiass <tiago.costadiasss@gmail.com>
2021-10-28 18:50:23 -0300

    feat: add vue2-transitions types, jest-dom types, and vue-good-table

---
+++ vue2-transitions.d.ts
@@ -0,0 +1,36 @@
+import { Component, PluginFunction } from 'vue';
+
+declare module 'vue2-transitions' {
+  declare const Transitions: Transitions;
+  export default Transitions;
+  export declare interface Transitions {
+    install: PluginFunction<TransitionOptions>;
+  }
+
+  export declare interface TransitionOptions {
+    components?: { name: TransitionName }[];
+  }
+
+  export type TransitionName =
+    | 'fade-transition'
+    | 'zoom-center-transition'
+    | 'zoom-x-transition'
+    | 'zoom-y-transition'
+    | 'collapse-transition'
+    | 'scale-transition'
+    | 'slide-y-up-transition'
+    | 'slide-y-down-transition'
+    | 'slide-x-left-transition'
+    | 'slide-x-right-transition';
+
+  export const FadeTransition: Component;
+  export const ZoomCenterTransition: Component;
+  export const ZoomXTransition: Component;
+  export const ZoomYTransition: Component;
+  export const CollapseTransition: Component;
+  export const ScaleTransition: Component;
+  export const SlideYUpTransition: Component;
+  export const SlideYDownTransition: Component;
+  export const SlideXLeftTransition: Component;
+  export const SlideXRightTransition: Component;
+}
